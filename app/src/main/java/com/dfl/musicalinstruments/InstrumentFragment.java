7a9d7556d6be50b0bb65db4bbf1ddfdc2b5bfc68
dellylucas <dellylucas@hotmail.com>
2019-07-26 19:16:50 -0500

    implemaentacion cambio de texto

--- app/src/main/java/com/dfl/musicalinstruments/InstrumentFragment.java
+++ app/src/main/java/com/dfl/musicalinstruments/InstrumentFragment.java
@@ -6,18 +6,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class InstrumentFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private static final String LLAVE_POSICION = "POSICION";
+    private int posicionActual = -1;
+
+    public static InstrumentFragment getInstance(int posicion) {
+        InstrumentFragment fragment = new InstrumentFragment();
+        Bundle argumentos = new Bundle();
+        argumentos.putInt(LLAVE_POSICION, posicion);
+        fragment.setArguments(argumentos);
+        return fragment;
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (savedInstanceState != null)
+            posicionActual = savedInstanceState.getInt(LLAVE_POSICION);
         return inflater.inflate(R.layout.fragment_instrument, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Bundle argumentos = getArguments();
+        if (argumentos != null)
+            actualizasVista(argumentos.getInt(LLAVE_POSICION));
+        else if (posicionActual != -1)
+            actualizasVista(posicionActual);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(LLAVE_POSICION, posicionActual);
+    }
+
+    public void actualizasVista(int posicion) {
+        TextView textoInformativo = (TextView) Objects.requireNonNull(getActivity()).findViewById(R.id.textInformative);
+        textoInformativo.setText("aaa");
+
+
+    }
 }
